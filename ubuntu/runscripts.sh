92b7c6c0516e2a41f547c695402ffeda5fc1c385
DBC <lancer1977@gmail.com>
2026-01-23 22:59:39 -0500

    lets avoid worrying here.

--- ubuntu/runscripts.sh
+++ ubuntu/runscripts.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
+set -e
 
-# Specify the directory containing the scripts
-script_directory="./scripts"
+script_directory="/scripts"
 
-# Check if the directory exists
 if [ -d "$script_directory" ]; then
-    # Loop through all executable files in the directory
-    for script_file in "$script_directory"/*; do
-        # Check if the file is executable
-        if [ -x "$script_file" ]; then
-            # Run the script
-            echo "Running script: $script_file"
-            "$script_file"
-        else
-            echo "Skipping non-executable file: $script_file"
-        fi
-    done
+  for script_file in "$script_directory"/*; do
+    [ -e "$script_file" ] || continue
+
+    if [ -x "$script_file" ]; then
+      echo "Running script: $script_file"
+      "$script_file"
+    else
+      echo "Skipping non-executable file: $script_file"
+    fi
+  done
 else
-    echo "Error: Directory not found - $script_directory"
+  echo "Error: Directory not found - $script_directory"
 fi
